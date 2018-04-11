b03c3af3e813789828a04112137fda234d6b3de5
Unknown <ariya.suthi@gmail.com>
2018-04-12 00:38:14 +0700

    add click(not finish)

--- build-tens-Desktop_Qt_5_10_0_MinGW_32bit-Debug/debug/moc_tens.cpp
+++ build-tens-Desktop_Qt_5_10_0_MinGW_32bit-Debug/debug/moc_tens.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_tens_t {
-    QByteArrayData data[1];
-    char stringdata0[5];
+    QByteArrayData data[6];
+    char stringdata0[85];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -31,10 +31,16 @@ struct qt_meta_stringdata_tens_t {
     )
 static const qt_meta_stringdata_tens_t qt_meta_stringdata_tens = {
     {
-QT_MOC_LITERAL(0, 0, 4) // "tens"
+QT_MOC_LITERAL(0, 0, 4), // "tens"
+QT_MOC_LITERAL(1, 5, 12), // "displayError"
+QT_MOC_LITERAL(2, 18, 0), // ""
+QT_MOC_LITERAL(3, 19, 28), // "QAbstractSocket::SocketError"
+QT_MOC_LITERAL(4, 48, 11), // "socketError"
+QT_MOC_LITERAL(5, 60, 24) // "on_ConnectButton_clicked"
 
     },
-    "tens"
+    "tens\0displayError\0\0QAbstractSocket::SocketError\0"
+    "socketError\0on_ConnectButton_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -44,22 +50,46 @@ static const uint qt_meta_data_tens[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    1,   24,    2, 0x08 /* Private */,
+       5,    0,   27,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void,
+
        0        // eod
 };
 
 void tens::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
-    Q_UNUSED(_a);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        tens *_t = static_cast<tens *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->displayError((*reinterpret_cast< QAbstractSocket::SocketError(*)>(_a[1]))); break;
+        case 1: _t->on_ConnectButton_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 0:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QAbstractSocket::SocketError >(); break;
+            }
+            break;
+        }
+    }
 }
 
 const QMetaObject tens::staticMetaObject = {
@@ -84,6 +114,17 @@ void *tens::qt_metacast(const char *_clname)
 int tens::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    }
     return _id;
 }
 QT_WARNING_POP
--- tens/tens.h
+++ tens/tens.h
@@ -2,7 +2,7 @@
 #define TENS_H
 
 #include <QWidget>
-//#include <QTcpSocket>
+#include <QTcpSocket>
 
 class tens : public QWidget
 {
@@ -11,18 +11,17 @@ class tens : public QWidget
 public:
     explicit tens(QWidget *parent = 0);
 
-    /*QTcpSocket *m_pClientsocket;
+    QTcpSocket *m_pClientsocket;
 
     //error connection
+
 private slots:
     void displayError ( QAbstractSocket::SocketError socketError );
 
-private slots:
-    void on_pushButton_clicked();
-
-    void on_connectButton_clicked();*/
+    void on_ConnectButton_clicked();
 
 private:
+
 
 };
 
